f5107f145288dae660ac7790fba7c02c44f421ad
Tattersson <tatupaski@outlook.com>
2021-01-16 08:23:53 +0200

    First compile worked

---
+++ RTGTV-Questions/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTGTV_Questions
+{
+    class Samsung
+    {
+    }
+}
---
+++ RTGTV-Questions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RTGTV_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heippa, tämä ohjelma toimii teille apuna ja toimittaa tiedot asennuksesta Klinikalle.");
+        }
+        
+    }
+}
