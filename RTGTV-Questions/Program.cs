1af023153dfe3334ff31dd3fbc4618b258eb0cc1
Tattersson <tatupaski@outlook.com>
2021-01-17 01:47:03 +0200

    Made comments on what to do

--- RTGTV-Questions/Program.cs
+++ RTGTV-Questions/Program.cs
@@ -9,10 +9,25 @@ namespace RTGTV_Questions
 
             Console.WriteLine("TV/AV - Kysely.");
             Console.Write("Anna kuitin tilausnumero: ");
-            Order();
+            //Kysy tilausnumero
             Console.Write("Mikä palvelu: ");
-
+            //Kysy palvelu
             Console.Write("Television merkki: ");
+            //Kysy television merkki. Jos Samsung niin kysy jatko kysymykset.
+            Console.Write("Television koko: ");
+            //Television koko.
+            Console.Write("Onko kuljetusta? (Y = Yes/ N = No)");
+            //Jos tulee kuljetus niin kysy kuljetuksen päivämäärä ja onko Lilli vai Koppo.
+            Console.Write("Toimistuspäivä: ");
+            //Toimitus päivän tarkastelu.
+            
+            //Tähän Kirjoita kaikki tiedot ja kysytään kirjoittajalta onko kaikki tiedot oikein.
+            //Jonka jälkeen ohjelma kirjoittaa kaikki tiedot .txt tiedostoon ja formatoi nimen.
+            //Esimerkiksi: 40702652652 - SATV36M - 25-01-2021
+
+
+            //TM RTGTV-Ohjelma - Tatu-Topias Paski - 2021
+
 
         }
         
